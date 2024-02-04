29b174add5234a1ff3ea7a7c1e3d59799f08d1e7
MASUDUR RAHMAN <masudparvez2050@gmail.com>
2024-02-04 11:42:21 +0600

    Final Update

--- frontend/utils/getsubcategory.js
+++ frontend/utils/getsubcategory.js
@@ -1,5 +1,8 @@
 export async function getSubCategory() {
-  const res = await fetch("http://localhost:3001/api/sub_categories");
+  // const res = await fetch("http://localhost:3001/api/sub_categories");
+  const res = await fetch(
+    "https://dua-api.masudur-rahman.xyz/api/sub_categories"
+  );
   const subcategories = await res.json();
 
   return {
